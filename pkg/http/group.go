7183007120114f5a063f3909252f679fdecc5dba
Michael Aldridge <aldridge.mac@gmail.com>
2022-01-23 23:13:41 -0600

    pkg/http: Add group edit form

--- pkg/http/group.go
+++ pkg/http/group.go
@@ -4,8 +4,23 @@ import (
 	"net/http"
 
 	"github.com/flosch/pongo2/v4"
+	"github.com/go-chi/chi/v5"
 	"github.com/netauth/netauth/pkg/netauth"
 )
+
+func (s *Server) viewGroupInfo(w http.ResponseWriter, r *http.Request) {
+	grp, mgd, err := s.c.GroupInfo(r.Context(), chi.URLParam(r, "name"))
+	if err != nil {
+		s.doTemplate(w, r, "errors/netauth.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+
+	ctx := pongo2.Context{
+		"group":   grp,
+		"managed": mgd,
+	}
+	s.doTemplate(w, r, "view/group_info.p2", ctx)
+}
 
 func (s *Server) viewGroupCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
@@ -19,10 +34,10 @@ func (s *Server) viewGroupCreate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fdata := struct {
-		Name     string
+		Name        string
 		DisplayName string
-		ManagedBy string
-		Number int
+		ManagedBy   string
+		Number      int
 	}{}
 
 	if err := s.f.Decode(&fdata, r.Form); err != nil {
@@ -36,4 +51,46 @@ func (s *Server) viewGroupCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	http.Redirect(w, r, "/group/info/"+fdata.Name, http.StatusSeeOther)
+}
+
+func (s *Server) viewGroupEditForm(w http.ResponseWriter, r *http.Request) {
+	grp, _, err := s.c.GroupInfo(r.Context(), chi.URLParam(r, "name"))
+	if err != nil {
+		s.doTemplate(w, r, "errors/netauth.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+
+	ctx := pongo2.Context{
+		"group": grp,
+	}
+	s.doTemplate(w, r, "view/group_edit.p2", ctx)
+}
+
+func (s *Server) viewGroupEditSubmit(w http.ResponseWriter, r *http.Request) {
+	gname := chi.URLParam(r, "name")
+
+	grp, _, err := s.c.GroupInfo(r.Context(), gname)
+	if err != nil {
+		s.doTemplate(w, r, "errors/netauth.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+
+	if err := r.ParseForm(); err != nil {
+		s.doTemplate(w, r, "errors/internal.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+
+	if err := s.f.Decode(&grp, r.Form); err != nil {
+		s.doTemplate(w, r, "errors/internal.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+	// Prevent any funny business...
+	grp.Name = &gname
+
+	ctx := netauth.Authorize(r.Context(), s.getToken(r))
+	if err := s.c.GroupUpdate(ctx, grp); err != nil {
+		s.doTemplate(w, r, "errors/netauth.p2", pongo2.Context{"error": err.Error()})
+		return
+	}
+	http.Redirect(w, r, "/group/info/"+gname, http.StatusSeeOther)
 }
--- pkg/http/server.go
+++ pkg/http/server.go
@@ -69,11 +69,12 @@ func New(l hclog.Logger) (*Server, error) {
 	s.r.Get("/entity/search", s.viewEntitySearch)
 	s.r.Get("/entity/info/{id}", s.viewEntityInfo)
 
-
 	s.r.Get("/group/new", s.viewGroupCreate)
 	s.r.Post("/group/new", s.viewGroupCreate)
 	s.r.Get("/group/search", s.viewGroupSearch)
 	s.r.Get("/group/info/{name}", s.viewGroupInfo)
+	s.r.Get("/group/edit/{name}", s.viewGroupEditForm)
+	s.r.Post("/group/edit/{name}", s.viewGroupEditSubmit)
 
 	return &s, nil
 }
